489baed570f66bff24ccbf4cb344ed4fac5f1e9f
nass202 <132716476+nass202@users.noreply.github.com>
2023-05-16 05:50:16 -0700

    add js
    add js

--- jin portfolio/js/scripts.js
+++ jin portfolio/js/scripts.js
@@ -1,1 +1,9 @@
 console.log("welcome to Jin's portfolio");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
